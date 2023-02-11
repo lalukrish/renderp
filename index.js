3f9035826d7065a5da663c43206ea0959c73587b
Lal krishna N C <lalukrishnavallil01@gmail.com>
2023-02-11 13:14:41 +0530

    work akane

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 
 const express = require("express");
 const app = express();
@@ -10,6 +11,18 @@ require("./model/user");
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/auth"));
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
 mongoose.connect(process.env.MONGOURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
